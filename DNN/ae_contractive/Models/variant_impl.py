abaec485a61d0edd70ba20bafffbf1345b580eaf
Anurag Basistha <anuragbasistha@gmail.com>
2025-12-05 17:07:52 +0530

    cleaned

--- DNN/ae_contractive/Models/variant_impl.py
+++
@@ -1,6 +0,0 @@
-﻿from Autoencoder.ae_graph_stl.Models.variant_impl import AEGraphModel
-
-
-class AEContractiveModel(AEGraphModel):
-    """Contractive autoencoder shares architecture with graph STL."""
-    pass
--- DNN/ae_robust/Models/variant_impl.py
+++
@@ -1,1 +0,0 @@
-﻿from Autoencoder.ae_graph_stl.Models.variant_impl import AEGraphModel as AERobustModel
